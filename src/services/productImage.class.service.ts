7fb7d81d40888c95eb58be8d91f3288913c26262
Phan_Tan_Loc <ginga550504@gmail.com>
2022-07-24 15:43:53 +0700

    update

--- src/services/productImage.class.service.ts
+++ src/services/productImage.class.service.ts
@@ -39,46 +39,17 @@ export class ProductImage extends CommonController {
     res: Response
   ): Promise<Response<any, Record<string, any>> | undefined> {
     try {
-      const _order = req.query._order as 'DESC' | 'ASC';
-      const name_order = req.query.name_order as string;
-      const name_like = req.query.name_like as string;
+      const productId = req.query.productId;
 
-      const _limit = parseInt(req.query._limit as string);
-      let _page = parseInt(req.query._page as string);
-
-      _page = _page < 0 ? (_page = 0) : _page;
-
-      if (!_limit && !_page && !_order && !name_order && !name_like) {
-        const response = await super.handleGetAll();
-        return res.status(200).json({
-          message: 'GET ALL SUCCEED.',
-          error: false,
-          data: response,
-          pagination: {
-            _limit: 5,
-            _page: 0,
-            _totalRows: response.length,
-          },
-        });
+      if (!productId) {
+        return res.status(404).json({ message: 'Missing parameter !', error: true });
       }
 
-      const filter: FilterPayload = {
-        ...req.query,
-        _limit,
-        _page,
-        name_query: 'thumb',
-        name_order,
-        _order,
-        name_like,
-      };
-
-      const response = await super.handleGetAllAndFilter(filter);
-
+      const response = await super.handleGetAll({ where: { productId } });
       res.status(200).json({
-        message: 'GET ALL SUCCEED',
+        message: 'GET ALL SUCCEED.',
         error: false,
-        data: response.rows,
-        pagination: { _limit: _limit, _page: _page, _totalRows: response.count },
+        data: response,
       });
     } catch (error) {
       log.error(error);
